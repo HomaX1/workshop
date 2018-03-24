84f6c9a5e3fc2c1df83254aad00269d5656c320a
HomaX1 <baitasova.anastasiia@gmail.com>
2018-03-24 21:07:52 +0200

    created project architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,41 @@
+'use strict';
+
 var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
     connect = require('gulp-connect'),
     sass = require('gulp-sass'),
-    imagemin = require('gulp-imagemin'),
-    watch = require('gulp-watch');
+    cleanCss = require('gulp-clean-css');
 
-gulp.task('sass', function() {
-    gulp.src('./src/css/scss/**/*.scss')
-        .pipe(sass({
-            includePaths: require('node-normalize-scss').includePaths
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('./build/css/'))
-        .pipe(connect.reload());
-});
 
 gulp.task('connect', function() {
     connect.server({
         port: 1703,
-        root: './build',
+        root: './src',
         livereload: true
     });
 });
 
-gulp.task('html', function () {
-    gulp.src('./src/*.html')
-        .pipe(gulp.dest('./build/'))
+gulp.task('sass', function() {
+    gulp.src('./src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./src/css/'))
         .pipe(connect.reload());
 });
 
-gulp.task('images', function() {
-    gulp.src('./src/images/**/*.*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./build/images/'))
-});
-
-gulp.task('fonts', function() {
-    gulp.src('./src/fonts/**/*.*')
-        .pipe(gulp.dest('./build/fonts/'))
+gulp.task('html', function () {
+    gulp.src('./src/*.html')
+        // .pipe(gulp.dest('./build/'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['./src/*.html'], ['html'])
-    gulp.watch('./src/css/scss/**/*.scss', ['sass'])
-    gulp.watch('./src/images/**/*.*', ['images'])
+    gulp.watch(['./src/*.html'], ['html']);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['connect', 'sass', 'images', 'fonts', 'watch']);
+gulp.task('default', ['connect', 'sass', 'html', 'watch']);
