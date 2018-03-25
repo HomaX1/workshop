bdb832b88ab0c9f0d72f7a0e71c6b50964fbeeaf
HomaX1 <baitasova.anastasiia@gmail.com>
2018-03-26 02:05:36 +0300

    changed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
-    rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     connect = require('gulp-connect'),
     sass = require('gulp-sass'),
-    cleanCss = require('gulp-clean-css');
+    cleanCss = require('gulp-clean-css'),
+    clean = require('gulp-clean');
 
 
-gulp.task('connect', function() {
+gulp.task('connect', function () {
     connect.server({
         port: 1703,
         root: './src',
@@ -16,7 +16,25 @@ gulp.task('connect', function() {
     });
 });
 
-gulp.task('sass', function() {
+gulp.task('clean', function () {
+    return gulp.src('./build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.src('./src/css/main.css')
+        .pipe(cleanCss())
+        .pipe(gulp.dest('build/css'));
+
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest('build'));
+    gulp.src('./src/fonts/**/*')
+        .pipe(gulp.dest('build/fonts'));
+    gulp.src('./src/img/*')
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('sass', function () {
     gulp.src('./src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
@@ -29,11 +47,10 @@ gulp.task('sass', function() {
 
 gulp.task('html', function () {
     gulp.src('./src/*.html')
-        // .pipe(gulp.dest('./build/'))
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(['./src/*.html'], ['html']);
     gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
